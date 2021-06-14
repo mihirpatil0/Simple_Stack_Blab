a3d1594bc16f6b30c001c820edbb9980b0ef8c00
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-15 02:41:17 +0530

    Calling: pop(),peek() and size() function.

--- Main.java
+++ Main.java
@@ -11,7 +11,24 @@ public class Main
 		simpleStackObject.push(30);
 		simpleStackObject.push(56);
 		
-		//printing stack.
+		//printing the size of stack after pushing elements inside.
+		System.out.println("Size of an stack is : " + simpleStackObject.size() + "\n");
+		
+		//printing full stack elements.
 		simpleStackObject.displayStack();
+		
+		//displaying only last i.e top element.
+		System.out.println("Peek element: " + simpleStackObject.peek() + "\n");
+		
+		//Popping i.e removing last inserted element from stack.
+		System.out.println("Popping element: " + simpleStackObject.pop() + "\n");
+		
+		//again printing size of an stack after popping element.
+		System.out.println("Size of an stack is : " + simpleStackObject.size() + "\n");
+		
+		//printing full stack elements.
+		simpleStackObject.displayStack();
+		
+		
 	}
 }

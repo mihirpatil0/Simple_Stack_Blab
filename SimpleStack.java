c3920bb27ad2565df4736e1869306d44cb49090c
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-15 02:07:28 +0530

    Added: push() function that will add push elements inside stack one above other.

--- SimpleStack.java
+++ SimpleStack.java
@@ -2,5 +2,32 @@ package com.bridgelab;
 
 public class SimpleStack 
 {
-
+	Node top;
+	int size;
+	
+	/**
+	 * @param element
+	 * element variable holds value that we want to push into stack.
+	 */
+	public void push(int element)
+	{
+		Node tempNode = new Node(element);
+		tempNode.next = top;
+		top = tempNode;
+		size++;
+	}
+	
+	/**
+	 * Printing Stack.
+	 */
+	public void displayStack()
+	{
+		Node curruent = top;
+		
+		while(curruent != null)
+		{
+			System.out.println(curruent.data);
+			curruent = curruent.next;
+		}
+	}
 }
